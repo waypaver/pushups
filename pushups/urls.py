5014a15bd5b2e34b88ecad9b9f27242bd1689798
Chris Keathley <spyc3r@gmail.com>
2014-09-05 11:08:49 -0400

    Fix import issue
    The main app was not being imported correctly

--- pushups/urls.py
+++ pushups/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from main import views
 
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'pushups.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-    url(r'^$', main.views.index, name='test_index'),
+    url(r'^$', views.index, name='test_index'),
     url(r'^admin/', include(admin.site.urls)),
 )
