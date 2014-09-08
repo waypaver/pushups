271f1eacd1931149b521267156db1c62733aba06
GEWalters <waltersge1@gmail.com>
2014-09-08 14:11:58 -0400

    Postgresql working. User and workout models are created, and they can be accessed via admin page.

--- main/models.py
+++ main/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib import admin
+from datetime import datetime
 
-# Create your models here.
+class User(models.Model):
+	phoneNumber = models.CharField(max_length=10,default="")
+	emailAddress = models.CharField(max_length=100,default="")
+	firstName = models.CharField(max_length=30,default="")
+	lastName = models.CharField(max_length=30,default="")
+	active = models.BooleanField(default=False)
+
+	def __unicode__(self):
+		return unicode(self.firstName + " " + self.lastName)
+
+class Workout(models.Model):
+	participantID = models.ForeignKey(User)
+	dateTimeStarted = models.DateTimeField(auto_now_add=True)
+	score = models.IntegerField(default=0)
+	status = models.CharField(max_length=10,default="pending")
+
+admin.site.register(User)
+admin.site.register(Workout)
