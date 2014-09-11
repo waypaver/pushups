af99345e75e73b67898aeb24edc7a5cc092672c3
Jodything <jodything@gmail.com>
2014-09-11 13:45:00 -0400

    Update urls.py

--- pushups/urls.py
+++ pushups/urls.py
@@ -4,23 +4,8 @@ from main import views
 from django.views.generic import TemplateView
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'pushups.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-    #url(r'^$', views.index, name='test_index'),
     url(r'^admin/', include(admin.site.urls)),
 
-    #(r'^accounts/', include('userprofile.urls')),
-    #user auth urls
-    #url(r'^accounts/login/$', 'main.views.login'),
-    #url(r'^accounts/auth/$', 'main.views.auth_view'),
-    #url(r'^accounts/logout/$', 'main.views.logout'),
-    #url(r'^accounts/loggedin/$', 'main.views.loggedin'),
-    #url(r'^accounts/invalid/$', 'main.views.invalid_login'),
-
-    #registration urls
-    #url(r'^accounts/register/$', 'main.views.register_user', name='register'),
-    #url(r'^accounts/register_success/$', 'main.views.register_success'),
     url(r'^sms/$', views.sms),
 
     url(r'^$',TemplateView.as_view(template_name='index.html'),name='index'),
